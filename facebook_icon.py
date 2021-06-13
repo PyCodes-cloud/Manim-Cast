0981e72a358924c626cb953c78560230fa0ef88f
CodeTech <84615187+PyCodes-cloud@users.noreply.github.com>
2021-06-13 11:05:19 +0530

    Update and rename test.py to facebook_icon.py

---
+++ facebook_icon.py
@@ -0,0 +1,25 @@
+from manim import *
+
+class FacebookIcon(Scene):
+	def construct(self):
+
+		my_circle = Circle(radius=1.6, color=DARK_BLUE, fill_opacity=1.0)
+		icon = Text("f", font='Calibri').scale(6.0)\
+							.set_color(WHITE)\
+							.move_to(my_circle)\
+							.shift(DOWN*0.25)
+
+		self.play(
+			GrowFromCenter(my_circle),
+			Write(icon),
+			run_time = 1.2
+		)
+		self.wait()
+
+		my_group = VGroup(my_circle, icon)
+
+		self.play(
+			my_group.animate.shift(UP+LEFT*2),
+			run_time=1.2
+		)
+		self.wait()
